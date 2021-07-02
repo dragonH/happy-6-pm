44e146c040220f6006042d0b1b5e5f7c166ea0bc
DragonH <a0261038@g2.usc.edu.tw>
2021-07-03 02:30:19 +0800

    refactoring code

--- src/handler.ts
+++ src/handler.ts
@@ -1,21 +1,80 @@
-// const chromium = require('chrome-aws-lambda');
 import { Authenticator } from 'otplib/core';
 import { keyDecoder, keyEncoder } from '@otplib/plugin-thirty-two';
 import { createDigest, createRandomBytes } from '@otplib/plugin-crypto';
+import { Browser, Page } from 'puppeteer-core';
 import chromium from 'chrome-aws-lambda';
 import moment from 'moment';
+import log4js, { Logger } from 'log4js';
 
+let logger: Logger;
+
+const initialLogger = async () => {
+    /**
+    *   This function is to initial logger
+    */
+    try {
+        logger = log4js.getLogger();
+        logger.level = 'info';
+        return logger;
+    } catch (error) {
+        logger.error(error);
+        throw new Error('Error while initial logger.');
+    }
+};
 const getRandomNumber = () => Math.ceil(Math.random() * 30);
 
-const sleep = async (seconds: number) => new Promise((resolve) => setTimeout(
+const sleep = async (_seconds: number) => new Promise((resolve) => setTimeout(
     /** This function is to wait for a period of time */
-    () => resolve(true), seconds * 1000,
+    () => resolve(true), _seconds * 1000,
 ));
+
+const initialBrowser = async () => {
+    /**
+    *   This function is to initial browser
+    */
+    try {
+        const browser = await chromium.puppeteer.launch({
+            args: [
+                ...chromium.args,
+                '--lang=en-US',
+            ],
+            defaultViewport: chromium.defaultViewport,
+            executablePath: await chromium.executablePath,
+            headless: true,
+            ignoreHTTPSErrors: true,
+        });
+        return browser;
+    } catch (error) {
+        logger.error(error);
+        throw new Error('Error while initial browser.');
+    }
+};
+
+const initialPage = async (
+    _browser: Browser,
+    _url: string,
+) => {
+    /**
+    *   This function is to initial page
+    */
+    try {
+        const page = (await _browser.pages())[0] || await _browser.newPage();
+        logger.info(`Navigate to ${_url}`);
+        await page.goto(_url, { waitUntil: 'networkidle0' });
+        logger.info(`Navigate to ${_url} succeed.`);
+        return page;
+    } catch (error) {
+        logger.error(error);
+        throw new Error('Error while initial page.');
+    }
+};
 
 const getMFAToken = async (
     MFASecret: string,
 ) => {
-    /** This function is to get mfa token */
+    /**
+    *   This function is to get mfa token
+    */
     try {
         const authenticator = new Authenticator({
             createDigest,
@@ -26,19 +85,225 @@ const getMFAToken = async (
         const token = authenticator.generate(MFASecret);
         return token;
     } catch (error) {
-        console.error(error);
-        throw Error('Error Whilee Get MAF Token.');
+        logger.error(error);
+        throw new Error('Error while get mfa token.');
+    }
+};
+
+const processLoginAction = async (
+    _page: Page,
+    _email: string,
+    _password: string,
+    _MFASecret: string,
+) => {
+    /**
+    *   This function is to process login action
+    */
+    try {
+        await _page.waitForSelector('#i0116', { timeout: 60000 });
+        await _page.focus('#i0116');
+        await _page.keyboard.type(_email);
+        logger.info(`Logging with username ${_email}`);
+        await _page.click('#idSIButton9');
+        await _page.waitForTimeout(1000);
+        await _page.waitForSelector('#i0118', { timeout: 60000 });
+        await _page.focus('#i0118');
+        await _page.keyboard.type(_password);
+        logger.info(`Logging with password ${'*'.repeat(_password.length)}`);
+        await _page.waitForSelector('input[value="Sign in"]', { timeout: 60000 });
+        await _page.waitForSelector('input[value="Sign in"]', { timeout: 60000 });
+        await _page.click('input[value="Sign in"]');
+        const MFAToken = await getMFAToken(_MFASecret);
+        await _page.waitForSelector('#idTxtBx_SAOTCC_OTC', { timeout: 60000 });
+        await _page.focus('#idTxtBx_SAOTCC_OTC');
+        await _page.keyboard.type(MFAToken);
+        logger.info(`Verify with mfa token ${MFAToken}`);
+        await _page.waitForSelector('input[value="Verify"]', { timeout: 60000 });
+        await _page.waitForSelector('input[value="Verify"]', { timeout: 60000 });
+        await _page.click('input[value="Verify"]');
+        logger.info('Log in succeed.');
+        await _page.waitForSelector('input[value="Yes"]', { timeout: 60000 });
+        await _page.click('input[value="Yes"]');
+        await _page.waitForTimeout(2000);
+    } catch (error) {
+        logger.error(error);
+        throw new Error('Error while process login action');
+    }
+};
+
+const getCorrentTime = async () => {
+    /**
+    *   This function is to get current
+    */
+    try {
+        const currentTime = moment();
+        // const currentTime = moment('2021-05-26 10:00');
+        logger.info(`Current time is ${currentTime.toDate()}`);
+        return currentTime;
+    } catch (error) {
+        logger.error(error);
+        throw new Error('Error while get current time.');
+    }
+};
+
+const processHealthStatusCheck = async (
+    _page: Page,
+) => {
+    try {
+        await _page.waitForSelector('input[value="正常"]', { timeout: 60000 });
+        await _page.click('input[value="正常"]');
+        logger.info('身體狀況選擇 正常');
+        await _page.waitForTimeout(1000);
+    } catch (error) {
+        logger.error(error);
+        throw new Error('Error while process health status check.');
+    }
+};
+
+const processOffWorkReport = async (
+    _page: Page,
+) => {
+    /**
+    *   This function is to process off work report
+    */
+    try {
+        logger.info('Processing off work report');
+        await _page.waitForSelector('input[value="晚上下班前填寫"]', { timeout: 60000 });
+        await _page.click('input[value="晚上下班前填寫"]');
+        logger.info('本次填寫時段選擇 晚上下班前填寫');
+        await _page.waitForTimeout(1000);
+        await _page.waitForSelector('input[value="直接回家不繞路"]', { timeout: 60000 });
+        await _page.click('input[value="直接回家不繞路"]');
+        logger.info('今晚預計移動地點選擇 直接回家不繞路');
+    } catch (error) {
+        logger.error(error);
+        throw new Error('Error while process off work report.');
+    }
+};
+
+const processBeforeWorkReportAfterHoliday = async (
+    _page: Page,
+) => {
+    /**
+    *   This function is to process before work report after holiday
+    */
+    try {
+        logger.info('Processing before work report after holiday');
+        await _page.waitForSelector('input[value="假日後上班(週一早上請選此項)"]', { timeout: 60000 });
+        await _page.click('input[value="假日後上班(週一早上請選此項)"]');
+        logger.info('本次填寫時段選擇 假日後上班(週一早上請選此項)');
+        await _page.waitForTimeout(1000);
+        await _page.waitForSelector('input[value="在家休息無外出"]', { timeout: 60000 });
+        await _page.click('input[value="在家休息無外出"]');
+        logger.info('假日活動地點選擇 在家休息無外出');
+        await _page.waitForTimeout(1000);
+        await _page.waitForSelector('input[value="在家工作"]', { timeout: 60000 });
+        await _page.click('input[value="在家工作"]');
+        logger.info('工作地點選擇 在家工作');
+    } catch (error) {
+        logger.error(error);
+        throw new Error('Error while process before work report after holiday.');
+    }
+};
+
+const processBeforeWorkReport = async (
+    _page: Page,
+) => {
+    /**
+    *   This function is to process before work report
+    */
+    try {
+        logger.info('Processing before work report');
+        await _page.waitForSelector('input[value="早上上班前填寫"]', { timeout: 60000 });
+        await _page.click('input[value="早上上班前填寫"]');
+        logger.info('本次填寫時段選擇 早上上班前填寫');
+        await _page.waitForTimeout(1000);
+        await _page.waitForSelector('input[value="在家工作"]', { timeout: 60000 });
+        await _page.click('input[value="在家工作"]');
+        logger.info('工作地點選擇 在家工作');
+    } catch (error) {
+        logger.error(error);
+        throw new Error('Error while process before work report.');
+    }
+};
+
+const processTimesheetReport = async (
+    _page: Page,
+) => {
+    /**
+    *   This function is to process timesheet report
+    */
+    try {
+        await _page.waitForTimeout(1000);
+        await _page.waitForSelector('input[value="免填"]', { timeout: 60000 });
+        await _page.click('input[value="免填"]');
+        logger.info('是否填寫工時表選擇 免填');
+    } catch (error) {
+        logger.error(error);
+        throw new Error('Error while process timesheet report.');
+    }
+};
+
+const processSpecialReport = async (
+    _page: Page,
+) => {
+    /**
+    *   This function is to process special report
+    */
+    try {
+        await _page.waitForTimeout(1000);
+        await _page.waitForSelector('input[value="否，從未出入該市場"]', { timeout: 60000 });
+        await _page.click('input[value="否，從未出入該市場"]');
+        logger.info('曾於6/8~7/1期間至環南市場 否，從未出入該市場');
+    } catch (error) {
+        logger.error(error);
+        throw new Error('Error while process special report.');
+    }
+};
+
+const processCheckReply = async (
+    _page: Page,
+) => {
+    /**
+    *   This function is to process check reply
+    */
+    try {
+        await _page.waitForTimeout(1000);
+        await _page.waitForSelector('input[type="checkbox"]', { timeout: 60000 });
+        await _page.click('input[type="checkbox"]');
+        logger.info('勾選回條');
+    } catch (error) {
+        logger.error(error);
+        throw new Error('Error while process check reply.');
+    }
+};
+
+const processSubmitReport = async (
+    _page: Page,
+) => {
+    /**
+    *   This function is to process submit report
+    */
+    try {
+        await _page.waitForSelector('button[title="Submit"]', { timeout: 60000 });
+        await _page.click('button[title="Submit"]');
+    } catch (error) {
+        logger.error(error);
+        throw new Error('Error while process submit report.');
     }
 };
 
 // eslint-disable-next-line import/prefer-default-export
 export const autoReport = async () => {
-    /** This function is to process auto report */
+    /**
+    *   This function is to process auto report
+    */
     try {
+        initialLogger();
         const randomSleepSeconds = getRandomNumber();
-        console.log(`[Message]: Starting to sleep for ${randomSleepSeconds} seconds`);
+        logger.info(`Starting to sleep for ${randomSleepSeconds} seconds`);
         await sleep(randomSleepSeconds);
-        console.log(`[Message]: Finish to sleep for ${randomSleepSeconds} seconds`);
+        logger.info(`Finish to sleep for ${randomSleepSeconds} seconds`);
         const {
             url,
             email,
@@ -48,101 +313,48 @@ export const autoReport = async () => {
         if (!url || !email || !password || !MFASecret) {
             throw new Error('Missing parameters.');
         }
-        const browser = await chromium.puppeteer.launch({
-            args: [
-                ...chromium.args,
-                '--lang=en-US',
-            ],
-            defaultViewport: chromium.defaultViewport,
-            executablePath: await chromium.executablePath,
-            headless: true,
-            // headless: false,
-            ignoreHTTPSErrors: true,
-        });
-        const page = (await browser.pages())[0] || await browser.newPage();
-        console.log(`[Message]: Navigate to ${url}`);
-        await page.goto(url, { waitUntil: 'networkidle0' });
-        console.log(`[Message]: Navigate to ${url} succeed.`);
-        await page.waitForSelector('#i0116', { timeout: 60000 });
-        await page.focus('#i0116');
-        await page.keyboard.type(email);
-        await page.click('#idSIButton9');
-        await page.waitForTimeout(1000);
-        await page.waitForSelector('#i0118', { timeout: 60000 });
-        await page.focus('#i0118');
-        await page.keyboard.type(password);
-        await page.waitForSelector('input[value="Sign in"]', { timeout: 60000 });
-        await page.waitForSelector('input[value="Sign in"]', { timeout: 60000 });
-        await page.click('input[value="Sign in"]');
-        const MFAToken = await getMFAToken(MFASecret);
-        await page.waitForSelector('#idTxtBx_SAOTCC_OTC', { timeout: 60000 });
-        await page.focus('#idTxtBx_SAOTCC_OTC');
-        await page.keyboard.type(MFAToken);
-        await page.waitForSelector('input[value="Verify"]', { timeout: 60000 });
-        await page.waitForSelector('input[value="Verify"]', { timeout: 60000 });
-        await page.click('input[value="Verify"]');
-        console.log('[Message]: Login successfully.');
-        await page.waitForSelector('input[value="Yes"]', { timeout: 60000 });
-        await page.click('input[value="Yes"]');
-        await page.waitForTimeout(2000);
-        const currentTime = moment();
-        // const currentTime = moment('2021-05-26 10:00');
-        console.log(`[Message]: Current time is ${currentTime.toDate()}`);
-        await page.waitForSelector('input[value="正常"]', { timeout: 60000 });
-        await page.click('input[value="正常"]');
-        console.log('[Message]: 身體狀況選擇 正常');
-        await page.waitForTimeout(1000);
-        console.log(currentTime.hours());
+        const browser = await initialBrowser();
+        const page = await initialPage(
+            browser,
+            url,
+        );
+        processLoginAction(
+            page,
+            email,
+            password,
+            MFASecret,
+        );
+        const currentTime = await getCorrentTime();
+        await processHealthStatusCheck(
+            page,
+        );
         if (currentTime.hours() > 2) {
-            console.log('[Message]: This is night report.');
-            await page.waitForSelector('input[value="晚上下班前填寫"]', { timeout: 60000 });
-            await page.click('input[value="晚上下班前填寫"]');
-            console.log('[Message]: 本次填寫時段選擇 晚上下班前填寫');
-            await page.waitForTimeout(1000);
-            await page.waitForSelector('input[value="直接回家不繞路"]', { timeout: 60000 });
-            await page.click('input[value="直接回家不繞路"]');
-            console.log('[Message]: 今晚預計移動地點選擇 直接回家不繞路');
+            await processOffWorkReport(
+                page,
+            );
         } else if (currentTime.day() === 1) {
-            console.log('[Message]: This is morning report on Monday.');
-            await page.waitForSelector('input[value="假日後上班(週一早上請選此項)"]', { timeout: 60000 });
-            await page.click('input[value="假日後上班(週一早上請選此項)"]');
-            console.log('[Message]: 本次填寫時段選擇 假日後上班(週一早上請選此項)');
-            await page.waitForTimeout(1000);
-            await page.waitForSelector('input[value="在家休息無外出"]', { timeout: 60000 });
-            await page.click('input[value="在家休息無外出"]');
-            console.log('[Message]: 假日活動地點選擇 在家休息無外出');
-            await page.waitForTimeout(1000);
-            await page.waitForSelector('input[value="在家工作"]', { timeout: 60000 });
-            await page.click('input[value="在家工作"]');
-            console.log('[Message]: 工作地點選擇 在家工作');
+            await processBeforeWorkReportAfterHoliday(
+                page,
+            );
         } else {
-            console.log('[Message]: This is morning report except on Monday.');
-            await page.waitForSelector('input[value="早上上班前填寫"]', { timeout: 60000 });
-            await page.click('input[value="早上上班前填寫"]');
-            console.log('[Message]: 本次填寫時段選擇 早上上班前填寫');
-            await page.waitForTimeout(1000);
-            await page.waitForSelector('input[value="在家工作"]', { timeout: 60000 });
-            await page.click('input[value="在家工作"]');
-            console.log('[Message]: 工作地點選擇 在家工作');
+            await processBeforeWorkReport(
+                page,
+            );
         }
-        await page.waitForTimeout(1000);
-        await page.waitForSelector('input[value="免填"]', { timeout: 60000 });
-        await page.click('input[value="免填"]');
-        console.log('[Message]: 是否填寫工時表選擇 免填');
-        if (currentTime.hours() > 2) {
-            await page.waitForTimeout(1000);
-            await page.waitForSelector('input[value="否，從未出入該市場"]', { timeout: 60000 });
-            await page.click('input[value="否，從未出入該市場"]');
-            console.log('[Message]: 曾於6/8~7/1期間至環南市場 否，從未出入該市場');
-        }
-        await page.waitForTimeout(1000);
-        await page.waitForSelector('input[type="checkbox"]', { timeout: 60000 });
-        await page.click('input[type="checkbox"]');
-        console.log('[Message]: 勾選回條');
-        await page.waitForSelector('button[title="Submit"]', { timeout: 60000 });
-        await page.click('button[title="Submit"]');
+        await processTimesheetReport(
+            page,
+        );
+        await processSpecialReport(
+            page,
+        );
+        await processCheckReply(
+            page,
+        );
+        await processSubmitReport(
+            page,
+        );
         await browser.close();
-        console.log('[Message]: Auto report succeed.');
+        logger.info('Auto report succeed');
     } catch (error) {
         console.error(error);
         process.exit(1);
